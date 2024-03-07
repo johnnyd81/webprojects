9378d1b9e8cc6fe8f8baf2d5278dbdbe3db00d83
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-03-07 18:27:47 +0200

    Update App.js

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 //the ProjectUI component renders the entire application
 import ProjectUI from './components/ProjectUI'
-
+//main file that renders the entire application
 function App() {
   return (
     <div>
