210f18683c80bf5a86c890a3fd438d5967dadec2
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-03-28 18:54:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-//middleware that shows the path and the method of each request
+//middleware that shows the path and the method of each request from the user
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
