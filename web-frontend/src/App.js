b051eb7800d0f37f2d8956aa975f3561e1bd1c24
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-05-29 11:32:13 +0200

    Update App.js

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ProjectUI from './components/ProjectUI'
 //the main file that renders the entire application 
+//this file is kept lean as it is the entry point of the application
 function App() {
   return (
     <div>
