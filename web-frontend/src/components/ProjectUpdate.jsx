de00ff9935695929078c01a58734a529e3a3d33f
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-08-02 20:03:55 +0200

    Update ProjectUpdate.jsx

--- web-frontend/src/components/ProjectUpdate.jsx
+++ web-frontend/src/components/ProjectUpdate.jsx
@@ -23,7 +23,7 @@ const ProjectUpdate = ({ project, handleUpdate, closeEdit }) => {
   };
 
   const handleEditSubmit = (e) => {
-    //prevent the browser refreshing itself when a form is submiited
+    //prevent the browser refreshing itself when a form is submitted
     e.preventDefault();
     if (editProject.title === "" || editProject.description === "") {
       alert("Please complete all the fields!!");
