88d132dcae0ac80ea97425395e8ec9256ca36ead
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-01-23 19:01:27 +0200

    Update ProjectUI.js

--- web-frontend/src/components/ProjectUI.js
+++ web-frontend/src/components/ProjectUI.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Header from "./Header";
 import "../App.css";
+// import the required components
 import ProjectForm from "./ProjectForm";
 import ProjectList from "./ProjectList";
 
