98d9c8a29a96bf59febcee72e00892d718845bdc
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-02-16 20:02:23 +0200

    Update index.js

--- web-frontend/src/index.js
+++ web-frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+// render the entire app from the root variable
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
