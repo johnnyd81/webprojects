5593d43f5930415689c84bc9e71b6cea584bd34a
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-03-25 19:17:05 +0200

    Update index.js

--- web-frontend/src/index.js
+++ web-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
+import App from './App' //import App.js
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 // render the entire app from the root variable i.e. the render method
