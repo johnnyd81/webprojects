8af49893e71b1b7ea757a7d3bc7dba48227d7f9f
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-04-21 10:53:07 +0200

    Update ProjectItem.jsx

--- web-frontend/src/components/ProjectItem.jsx
+++ web-frontend/src/components/ProjectItem.jsx
@@ -5,7 +5,7 @@ const ProjectItem = ({ project, onDelete, onUpdate }) => {
   //openUpdate is a state variable that controls the opening and closing of the update component, that updates a web project
   const [openUpdate, setOpenUpdate] = useState(false); // it is false initially meaning the modal is closed
   
-  //opens the update modal if the edit button is clicked for that specific ProjectItem
+  //opens the update modal if the edit button is clicked for that specific ProjectItem element
   const handleEditClick = () => {
     setOpenUpdate(true); // opens the update project modal
   };
