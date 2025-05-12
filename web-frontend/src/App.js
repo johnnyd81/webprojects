6e89519e3eb1b015160e32a80b6b17b625ea687c
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-05-12 10:50:32 +0200

    Update App.js

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ProjectUI from './components/ProjectUI'
 
 //this is the main file that renders the entire app i.e. the ProjectUI component that contains all the component files
-//this file is kept minimal as it is the entry point of the web application
+//this file is kept minimal and it is the entry point of the web application
 
 function App() {
   return (
