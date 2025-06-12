178584d57687a8f2f2bebcc9183eae3cf46d7f4b
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-06-12 10:42:01 +0200

    Update Header.js

--- web-frontend/src/components/Header.js
+++ web-frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-//the header component that displays the main title for the application
+//the header component that displays the main title for the web application
 const Header = () => {
   return (
     <div className="header">
